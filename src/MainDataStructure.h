7c261a4e8f62f2302daaddbec5033d8226ffddf4
Daniel Belokon <daniel.belokon@gmail.com>
2022-05-28 12:25:27 +0300

    add maindatastructure.h

---
+++ src/MainDataStructure.h
@@ -0,0 +1,53 @@
+// this is the main system
+
+#ifndef MAINDATASTRCTURE_H
+#define MAINDATASTRCTURE_H
+#include "Company.h"
+#include "Employee.h"
+#include "AVLTree.h"
+#include "UnionFind.h"
+#include "HashTable.h"
+#include "Exceptions.h"
+
+class MainDataStructure
+{
+
+private:
+    // AVLTree<Company *> companies_tree;
+    // AVLTree<Company *> companies_with_employees_tree;
+    // AVLTree<Employee *> employees_tree;
+    // AVLTree<Employee *> employees_tree_by_salary;
+    // Employee *highest_earner;
+    UnionFind<Company> companies;
+    HashTable<Employee *> employees;
+    AVLTree<Employee *> employees_tree_by_salary;
+
+public:
+    MainDataStructure();
+    void AddCompany(int companyID, int value);
+    // void RemoveCompany(int companyID, bool force = false);
+    void AddEmployee(int employeeID, int companyID, int grade);
+    void RemoveEmployee(int employeeID);
+    bool AcquireCompany(int companyId, int aquiredCompanyId, double factor);
+
+    // int GetHighestEarner(int companyId);
+    // void setHighesEarner(Employee *emp);
+    int GetAllEmployeesBySalary(int companyId, int **employees); // returns num of employees
+    void GetHighestEarnerInEachCompany(int numOfCompanies, int **highestEarners);
+    int GetNumEmployeesMatching(int companyId, int minId, int maxId, int minSalary, int minGrad, int *inRange);
+
+    void GetCompanyInfo(int companyId, int *value, int *numOfEmployees);
+    void GetEmployeeInfo(int employeeID, int *employerID, int *salary, int *grade);
+    void IncreaseCompanyValue(int companyID, int valueIncrease);
+    void PromoteEmployee(int EmployeeID, int salaryIncrease, int bumpGrade);
+    void HireEmployee(int EmployeeID, int newCompanyID);
+
+    ~MainDataStructure();
+
+private:
+    void checkInRangeRocourisve(Node<Employee *> *current, int minId, int maxId, int minSalary, int minGrade, int *inRange, int *numOfEmployees);
+    Company *findCompanyById(int id);
+    Employee *findEmployeeById(int id);
+};
+
+#endif
