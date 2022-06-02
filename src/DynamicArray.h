b23180f9453c2a986e53d5bf8f45ea40367a9d3e
Daniel Belokon <daniel.belokon@gmail.com>
2022-06-02 17:35:08 +0300

    update dynamic array

--- src/DynamicArray.h
+++ src/DynamicArray.h
@@ -1,6 +1,8 @@
 #ifndef DYNAMICARRAY_H
 #define DYNAMICARRAY_H
 
+#include "Exceptions.h"
+
 #define DEFAULT_CAPACITY 8
 
 template <class T>
@@ -13,20 +15,19 @@ private:
     T *array;
 
 public:
-    DynamicArray(int capacity = DEFAULT_CAPACITY, int *customHash(int) = void);
+    DynamicArray(int capacity = DEFAULT_CAPACITY);
     void insert(int index, T value);
     void remove(int index);
     void resize();
-
+    ~DynamicArray();
     T &operator[](int id);
     T &operator[](int id) const;
 };
 
 template <class T>
-DynamicArray<T>::DynamicArray(int capacity, int *customHash(int))
+DynamicArray<T>::DynamicArray(int capacity)
 {
     this->capacity = capacity;
-    this->customHash = customHash;
     this->cur_size = 0;
     this->array = new T[capacity];
 }
@@ -34,12 +35,13 @@ DynamicArray<T>::DynamicArray(int capacity, int *customHash(int))
 template <class T>
 void DynamicArray<T>::insert(int index, T value)
 {
-    if (index > cur_size || index < 0)
+    if (index > 2 * capacity || index < 0)
     {
-        return; // TODO: Throw exception
+        throw InvalidInputException();
+        // return; // TODO: Throw exception
     }
 
-    if (size == capacity)
+    if (cur_size >= capacity)
     {
         resize();
     }
@@ -50,7 +52,7 @@ void DynamicArray<T>::insert(int index, T value)
 template <class T>
 void DynamicArray<T>::remove(int index)
 {
-    if (index > cur_size || index < 0)
+    if (index > 2 * capacity || index < 0)
     {
         return; // TODO: Throw exception
     }
@@ -63,12 +65,12 @@ void DynamicArray<T>::resize()
 {
     capacity *= 2;
     T *new_array = new T[capacity];
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < capacity; i++)
     {
         new_array[i] = array[i];
     }
     delete[] array;
-    array = new_array;
+    *array = *new_array;
 }
 
 template <class T>
@@ -81,6 +83,12 @@ template <class T>
 T &DynamicArray<T>::operator[](int id) const
 {
     return array[id];
+}
+// DESTRUCTOR
+template <class T>
+DynamicArray<T>::~DynamicArray()
+{
+    delete[] array;
 }
 
 #endif /* DYNAMICARRAY_H */
